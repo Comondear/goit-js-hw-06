25c3c9624395ac8f76a6ce7a596efd5b1db3800b
EL Gaidai <elcomondear@gmail.com>
2022-09-14 17:08:12 -0600

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,29 @@
-const refs = document.querySelector('#validation-input');
+// const refs = document.querySelector('#validation-input');
 
-const lengthReg = refs.getAttribute('data-length');
+// const lengthReg = refs.getAttribute('data-length');
 
-refs.addEventListener = ('blur', onInputBlur);
+// refs.addEventListener = ('blur', onInputBlur);
 
-function onInputBlur(event) {
-    if (event.currentTarget.value.length === Number(lengthReg)) {
-        event.currentTarget.classList.add('.valid');
-        event.currentTarget.classList.remove('.invalid');
-    } else {
-        event.currentTarget.classlist.add('.invalid');
-        event.currentTarget.classList.remove('.valid');
-        }
-    }
+// function onInputBlur(event) {
+//     if (event.currentTarget.value.length === Number(lengthReg)) {
+//         event.currentTarget.classList.add('valid');
+//         event.currentTarget.classList.remove('invalid');
+//     } else {
+//         event.currentTarget.classlist.add('invalid');
+//         event.currentTarget.classList.remove('valid');
+//         }
+// }
+    
+const inputRef = document.querySelector('#validation-input');
+
+const inputBlur = (event) => {
+  const { dadaset, value } = event.target;
+  const requiredLength = Number(dadaset.length);
+  if (requiredLength === value.length) {
+    event.target.classList.add('valid');
+    event.target.classList.remove('invalid');
+  } else {
+    event.target.classlist.add('invalid');
+    event.target.classList.remove('valid');
+  }
+}
